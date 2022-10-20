b6c83d5b413f1ce312bfefedac33d0ac13e41b4c
Jasser Tayari <jassertayari3@gmail.com>
2022-10-20 14:13:08 +0100

    seperated the scrollframe  with new class

--- classes.py
+++ classes.py
@@ -2,20 +2,11 @@ import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
-
-class Runner(tk.Tk):
+class ScrollFrame(tk.Frame):
     def __init__(self):
         super().__init__()
-        self.labels = (0,[])
-        self.rowconfigure(0,minsize=450,weight=1)
-        self.rowconfigure(1,minsize=50)
-        
-        self.columnconfigure(0,weight=1,minsize=500)
-        #           creating a scrollbar
-        #------------------------------------
-        self.container = tk.Frame(self)
-        self.canvas = tk.Canvas(self.container)
-        self.scrollbar = tk.Scrollbar(self.container, orient="vertical", command=self.canvas.yview)
+        self.canvas = tk.Canvas(self)
+        self.scrollbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
         self.scrollable_frame = tk.Frame(self.canvas)
 
         self.scrollable_frame.bind(
@@ -28,17 +19,23 @@ class Runner(tk.Tk):
         self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
 
         self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        #for i in range(50):
-        #    if(i==0):
-        #        lbl = tk.Label(self.scrollable_frame, text="Sample scrolling label",bg="grey")
-        #    else:    
-        #        lbl = tk.Label(self.scrollable_frame, text="Sample scrolling label",bg="white")
-        #    lbl.pack()
-        #    self.labels[1].append(lbl)
+        
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+class Runner(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.labels = (0,[])
+        self.rowconfigure(0,minsize=450,weight=1)
+        self.rowconfigure(1,minsize=50)
+        
+        self.columnconfigure(0,weight=1,minsize=500)
+        #           creating a scrollbar
+        #------------------------------------
+        self.container = ScrollFrame()
 
         self.container.grid(row=0,column=0,sticky="nsew")
-        self.canvas.pack(side="left", fill=tk.BOTH, expand=True)
-        self.scrollbar.pack(side="right", fill=tk.Y)
         #---------------------------------------------------
         self.btn_frm = tk.Frame(self)
         self.btn_frm.grid(row=1,column=0,sticky="nsew")
@@ -63,8 +60,7 @@ class Runner(tk.Tk):
     def openfile(self):
         filename = filedialog.askopenfilename(initialdir="/",filetypes=(("executables","*.sh"),("any","*.*") ) )
         if filename:
-            print("entered")
-            lbl = tk.Label(self.scrollable_frame, text=filename)
+            lbl = tk.Label(self.container.scrollable_frame, text=filename)
             lbl.configure(bg=("white" if len(self.labels[1]) else "grey") )
             self.labels[1].append(lbl)
             lbl.pack()
